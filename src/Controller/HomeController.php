2dfb3ebb753aebe2312df2789475f00beaf78601
Maxime Doutreluingne <53557695+mdoutreluingne@users.noreply.github.com>
2020-08-30 12:10:49 +0200

    Composer update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\PropertyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,6 +17,7 @@ class HomeController extends AbstractController
      */
     public function index(PropertyRepository $repository): Response
     {
+        
         $properties = $repository->findLatest();
         return $this->render('pages/home.html.twig', [
             'properties' => $properties
