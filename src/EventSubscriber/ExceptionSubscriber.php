0a7a60d58eaf23f1fafaa4b442477d1b93135276
Maxime Doutreluingne <maxime.doutreluingne@sfr.fr>
2020-10-06 14:51:58 +0200

    feat: update system log

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -34,14 +34,28 @@ class ExceptionSubscriber implements EventSubscriberInterface
         // holds status code and header details
         if ($exception instanceof HttpExceptionInterface) {
             switch (true) {
-            case ($exception->getStatusCode() > 400 && $exception->getStatusCode() < 500):
-                $this->logger->error($exception->getMessage());
-                break;
-
-            case ($exception->getStatusCode() > 200 && $exception->getStatusCode() < 250):
-                $this->logger->info($exception->getMessage());
-                break;
-        }
+                case ($exception->getStatusCode() >= 600):
+                    $this->logger->emergency($exception->getMessage());
+                    break;
+                case ($exception->getStatusCode() >= 550 && $exception->getStatusCode() < 600):
+                    $this->logger->alert($exception->getMessage());
+                    break;
+                case ($exception->getStatusCode() >= 500 && $exception->getStatusCode() < 550):
+                    $this->logger->critical($exception->getMessage());
+                    break;
+                case ($exception->getStatusCode() >= 400 && $exception->getStatusCode() < 500):
+                    $this->logger->error($exception->getMessage());
+                    break;
+                case ($exception->getStatusCode() >= 300 && $exception->getStatusCode() < 400):
+                    $this->logger->warning($exception->getMessage());
+                    break;
+                case ($exception->getStatusCode() >= 250 && $exception->getStatusCode() < 300):
+                    $this->logger->notice($exception->getMessage());
+                    break;
+                case ($exception->getStatusCode() >= 200 && $exception->getStatusCode() < 250):
+                    $this->logger->info($exception->getMessage());
+                    break;
+            }
         } else {
             $this->logger->info($exception->getMessage());
         }
