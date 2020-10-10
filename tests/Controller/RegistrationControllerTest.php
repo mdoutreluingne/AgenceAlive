3f94b4fbbe8a06a9b8dbac502317336cd325ce8b
Maxime Doutreluingne <maxime.doutreluingne@sfr.fr>
2020-10-10 17:15:19 +0200

    Add comment test class & update travis file

--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -11,16 +11,20 @@ class RegistrationControllerTest extends WebTestCase
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
+        //Clique sur le lien S'inscrire de la homepage
         $link = $crawler->selectLink("S'inscrire")->link();
         $crawler = $client->click($link);
 
+        //Remplie les champs du formulaire
         $form = $crawler->selectButton('Valider')->form();
         $form['registration_form[username]'] = "Cartman";
         $form['registration_form[email]'] = "cartman.st@gmail.com";
         $form['registration_form[plainPassword]'] = "cartman";
         $form['registration_form[agreeTerms]'] = true;
 
+        //Envoie les données au formulaire
         $client->submit($form);
+        //Redige après la soumission du formulaire
         $crawler = $client->followRedirect();
 
         $this->assertSame(1, $crawler->filter('div.alert.alert-danger')->count());
