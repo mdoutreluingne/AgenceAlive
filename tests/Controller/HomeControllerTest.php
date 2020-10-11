3352a75e6e2f5b58ade21acd41d82912407361e0
Maxime Doutreluingne <maxime.doutreluingne@sfr.fr>
2020-10-11 11:39:45 +0200

    test

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -3,15 +3,27 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeControllerTest extends WebTestCase
 {
-    public function testHomePage()
+    public function testHomePageIsUp()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        //$this->assertSame(200, $client->getResponse()->getStatusCode());
+
+        //echo $client->getResponse()->getContent();
+    }
+
+    /*public function testHomePage()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertSame(1, $crawler->filter('html:contains("Agence alive")')->count());
 
-    }
+    }*/
 }
