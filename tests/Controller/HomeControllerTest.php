035dbfb88bd3977a637ffec9c455317fd089f71e
Maxime Doutreluingne <maxime.doutreluingne@sfr.fr>
2020-10-11 18:25:39 +0200

    Add new function test

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -2,13 +2,33 @@
 
 namespace App\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Monolog\Handler\Handler;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
 {
+    protected function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+        $this->app->instance(ExceptionHandler::class, new class extends Handler
+        {
+            public function __construct()
+            {
+            }
+            public function report(\Exception $e)
+            {
+            }
+            public function render($request, \Exception $e)
+            {
+                throw $e;
+            }
+        });
+    }
+    
     public function testHomePageIsUp()
     {
+        $this->disableExceptionHandling();
         $client = static::createClient();
         $client->request('GET', '/');
 
@@ -19,6 +39,7 @@ class HomeControllerTest extends WebTestCase
 
     public function testHomePage()
     {
+        $this->disableExceptionHandling();
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
