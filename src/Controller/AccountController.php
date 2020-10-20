23d4aa1cc03eef02a3381e318d01bfd3bebc9f79
Maxime Doutreluingne <maxime.doutreluingne@sfr.fr>
2020-10-20 11:51:09 +0200

    feat: Add accountcontroller

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\EditAccountType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountController extends AbstractController
+{
+    /**
+     * @Route("/account", name="account")
+     */
+    public function index()
+    {
+        return $this->render('account/index.html.twig', [
+            'controller_name' => 'AccountController',
+        ]);
+    }
+
+    /**
+     * @Route("/account/edit", name="account_edit")
+     */
+    public function edit(Request $request)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditAccountType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('message', 'Profil mis à jour');
+            return $this->redirectToRoute('account');
+        }
+        return $this->render('account/editaccount.html.twig', [
+            'controller_name' => 'AccountController',
+        ]);
+    }
+}
