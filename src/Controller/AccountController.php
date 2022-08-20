42e66089cf59697eafd0037f4db06963f50588ba
Maxime Doutreluingne <maxime.doutreluingne@gmail.com>
2022-08-20 17:36:48 +0200

    Add 2 methods for set locale after update account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -6,10 +6,11 @@ use App\Form\EditAccountType;
 use App\Manager\BadgeManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class AccountController extends AbstractController
 {
@@ -39,20 +40,30 @@ class AccountController extends AbstractController
     /**
      * @Route("/account/edit/profile", name="account_edit_profile")
      */
-    public function editProfile(Request $request, TokenStorageInterface $tokenStorageInterface)
+    public function editProfile(Request $request, TokenStorageInterface $tokenStorageInterface, SessionInterface $sessionInterface)
     {
         $user = $this->getUser();
         $form = $this->createForm(EditAccountType::class, $user);
         $form->handleRequest($request);
+        
 
         if ($form->isSubmitted() && $form->isValid()) {
+            if ($user->getLocale() !== $sessionInterface->get('_locale')) {
+               $sessionInterface->set('_locale', $user->getLocale());
+            }
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
-            $entityManager->flush();
+            $entityManager->flush(); 
 
-            // $this->addFlash('success', 'Profil mis à jour');
-            $tokenStorageInterface->setToken();
-            return $this->redirectToRoute('app_logout');
+            // Method 1 : Force logout user 
+            // $tokenStorageInterface->setToken();
+            // return $this->redirectToRoute('app_logout');
+
+            // Method 2 : set locale if it has changed
+            $this->addFlash('success', 'Profil mis à jour');
+
+            return $this->redirectToRoute('account');
         }
 
         return $this->render('account/editaccount.html.twig', [
