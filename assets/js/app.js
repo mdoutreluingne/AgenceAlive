b412d7de7df4d47504b037d665deabfefa91eb83
Maxime Doutreluingne <maxime.doutreluingne@gmail.com>
2022-11-01 17:18:43 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
-var Translator = require('bazinga-translator');
+import Translator from 'bazinga-translator';
 Translator.add("foo", "Baaar", "messages", "fr");
 console.log(Translator.trans('foo'));
 
